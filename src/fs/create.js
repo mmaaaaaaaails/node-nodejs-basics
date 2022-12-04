6bc61144dd82dd966bdaba65e2036d12e45dba54
vadimdudko <v.dudko93@gmail.com>
2022-12-04 16:49:30 +0300

    init: start task create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+    // Write your code here
+
+    fs.writeFile(
+        path.join(__dirname, 'files', 'fresh.txt'),
+        'I am fresh and young',
+        (error) => {
+            if (error) throw error;
+            console.log('FS operation failed');
+        }
+    )
 };
 
 await create();
