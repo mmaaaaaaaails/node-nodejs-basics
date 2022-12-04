8d007d24bcf4fb687eacfb6e285ebb39120fd4ea
vadimdudko <v.dudko93@gmail.com>
2022-12-04 17:20:57 +0300

    init: start task delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+
 const remove = async () => {
-    // Write your code here 
+    // Write your code here
+
+    try {
+        fs.unlinkSync('src/fs/files/fileToRemove.txt');
+        console.log('FS operation failed')
+    } catch (error) {
+        console.log(error)
+    }
 };
 
 await remove();
