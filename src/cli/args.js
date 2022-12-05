9aa5f8959239492fe4575a6af2f610830e34a240
vadimdudko <v.dudko93@gmail.com>
2022-12-05 15:27:23 +0300

    init: start task args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+
+    const args = process.argv.slice(2);
+    const result = [];
+
+    for (let i = 0; i < args.length; i+=2) {
+        result.push(`${args[i].slice(2)} is ${args[i+1]}`)
+    }
+
+    return console.log(result.join(', '));
 };
 
 parseArgs();
