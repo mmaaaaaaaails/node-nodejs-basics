5bcfb13b1c97e8f706d8402cfbb39daf98642cee
vadimdudko <v.dudko93@gmail.com>
2022-12-05 16:56:03 +0300

    init: start task streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
+import fs from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+
+    const { stdout } = process;
+    const readableStream = fs.createReadStream('src/streams/files/fileToRead.txt', 'utf-8');
+    readableStream.on('data', chunk => stdout.write(chunk));
 };
 
 await read();
