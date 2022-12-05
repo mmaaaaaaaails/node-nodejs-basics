2c1b8a12ba326cb455194e282045982e826bf939
vadimdudko <v.dudko93@gmail.com>
2022-12-05 19:49:39 +0300

    init: start task streams/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    // Write your code here
+
+    const reverseStream = new Transform({
+        transform (data) {
+            const reversedData = data.toString().split("").reverse().join("");
+            this.push(reversedData);
+        }
+    });
+
+    process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
