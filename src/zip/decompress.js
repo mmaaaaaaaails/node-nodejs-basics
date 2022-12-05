5e988e17c0573bf2d282d1838d4ce693cc31d49a
vadimdudko <v.dudko93@gmail.com>
2022-12-05 20:57:50 +0300

    init: start task compress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import zlib from "node:zlib";
+import fs from "node:fs";
+
 const decompress = async () => {
-    // Write your code here 
+    // Write your code here
+
+    const gzip = zlib.createUnzip();
+    const inputFile = fs.createReadStream('src/zip/files/archive.gz');
+    const outputFile = fs.createWriteStream('src/zip/files/fileToCompress2.txt');
+
+    inputFile.pipe(gzip).pipe(outputFile);
 };
 
 await decompress();
