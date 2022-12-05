e0cddf128baa0c3de14db3322938227b240e2de0
vadimdudko <v.dudko93@gmail.com>
2022-12-05 17:27:12 +0300

    init: start task streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import fs from 'node:fs';
+
 const write = async () => {
-    // Write your code here 
+    // Write your code here
+
+    const { stdin } = process;
+    const output = fs.createWriteStream('src/streams/files/fileToWrite.txt', 'utf-8');
+    stdin.pipe(output);
 };
 
 await write();
