bd9e9c528363d474efa266a880f547db6686ac96
vadimdudko <v.dudko93@gmail.com>
2022-12-05 13:17:14 +0300

    init: start task read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        const content = fs.readFileSync('src/fs/files/fileToRead.txt', 'utf-8');
+        console.log(content)
+    } catch (e) {
+        console.log('FS operation failed')
+    }
 };
 
 await read();
